5347d17e0074f9582d69807506f1df20de9bdee3
unknown <urmi1907094@stud.kuet.ac.bd>
2023-04-06 16:19:51 +0600

    unit test

--- app/src/main/java/com/example/citylist/CityList.java
+++ app/src/main/java/com/example/citylist/CityList.java
@@ -2,17 +2,18 @@ package com.example.citylist;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-/**
- * This is a class that keeps track of a list of city objects
+
+        /* This is a class that keeps track of a list of city objects
  */
 public class CityList {
     private List<City> cities = new ArrayList<>();
 
-    /**
-     * This adds a city to the list if that city does not exist
-     * @param city
+
+            /* This adds a city to the list if that city does not exist
+     * @param city @param city form add()
      *      This is the city to add
      */
     public void add(City city) {
@@ -22,14 +23,59 @@ public class CityList {
         cities.add(city);
     }
 
-    /**
-     * This returns a sorted list of cities
-     * @return
-     *      Return the sorted list of cities
-     */
+
+          /*  returns a sorted list of cities
+     * @return return getCities()
+     *
+             */
     public List<City> getCities() {
         List<City> cityList = cities;
         Collections.sort(cityList);
         return cityList;
     }
+
+    /*
+            * @param city  deletes city
+     */
+    //solution of Task i
+    public void delete(City city){
+        if(!cities.contains(city)){
+            throw new IllegalArgumentException();
+        }
+        else{
+            cities.remove(city);
+        }
+    }
+
+    /*
+            * @return
+            * returns how many cities
+     */
+    //solution of Task ii
+    public int count(){
+        return cities.size();
+    }
+
+    /*  returns the sorted list
+     * @param flag  sort CityList based on City name and Province name
+     * @return  getCitiesSort
+     */
+    //solution of Task iii
+    public List<City> getCitiesSort(int flag) {
+        List<City> cityList = cities;
+        if(flag==1){
+            Collections.sort(cityList);
+        }
+        else{
+            Collections.sort(cityList, new Comparator<City>() {
+                @Override
+                public int compare(City city, City t1) {
+                    return city.getProvinceName().compareTo(t1.getProvinceName());
+                }
+            });
+        }
+
+        return cityList;
+    }
+
 }
